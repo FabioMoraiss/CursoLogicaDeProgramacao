ec4a45fab05eb87c5601d4bcecacced44e79201c
Fabio Morais <104800091+FabioMoraiss@users.noreply.github.com>
2024-09-02 05:15:40 +0000

    criação do arquivo variaveis, onde eu aprendi oque sao variaveis

---
+++ aula_3/variaveis.java
@@ -0,0 +1,13 @@
+public class variaveis {
+    public static void main(String[] args) {
+        int numeroUm = 10;
+        int numeroDois = 100;
+        int numeroTres = 300;
+        int resultado = numeroUm + numeroDois + numeroDois + numeroTres;
+        System.out.println(resultado);
+
+        final int teste1 = 27;
+        // hoje eu comi hamburger no cafe da manha
+        System.out.println(teste1);
+    }
+}
